91409fbaf2973d8f89d3152606338da235eaf9bf
Khushi Shetiya <khushishetiya13@gmail.com>
2026-06-28 18:47:11 +0530

    Setup useState, onChange/OnClick handlers, and parent-child props flow

--- src/components/NoteInput.jsx
+++ src/components/NoteInput.jsx
@@ -1,10 +1,23 @@
-export default function NoteInput() {
+import { useState } from "react";
+
+export default function NoteInput({onAdd}) {
+    const [text, setText] = useState("");
+
+    let handleChange = (event) => {
+        setText(event.target.value);
+    };
+
+    let handleAdd = () => {
+        if(text.trim() === "") return;
+        onAdd(text);
+        setText("");
+    };
     return (
         <>
             <div>
-                <input placeholder="Write note...."/>
-                <button>Add</button>
+                <input placeholder="Write note...." type="text" value={text} onChange={handleChange}/>
+                <button onClick={handleAdd}>Add</button>
             </div>
         </>
-    )
+    );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import Navbar from "../components/Navbar";
 import NoteInput from "../components/NoteInput";
 import NoteList from "../components/NoteList";
 
 export default function Home () {
+    const[notes, setNotes] = useState([]);
+
+    const addNote = (text) => {
+        setNotes((prev) => [...prev, text]);
+    };
     return (
         <>
             <Navbar/>
-            <NoteInput/>
-            <NoteList/>
+            <NoteInput onAdd={addNote}/>
+            <NoteList notes={notes}/>
         </>
     );
 }
