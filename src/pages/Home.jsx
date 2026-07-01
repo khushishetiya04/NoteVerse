2b784fcbeb6fe63b52abf3d5144c5f426fb5dc30
Khushi Shetiya <khushishetiya13@gmail.com>
2026-07-01 21:48:05 +0530

    Implemented localStorage and note state management

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,19 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
 import NoteInput from "../components/NoteInput";
 import NoteList from "../components/NoteList";
 
 export default function Home () {
-    const[notes, setNotes] = useState([]);
+    const[notes, setNotes] = useState( JSON.parse(localStorage.getItem("notes")) || []);
 
     const addNote = (text) => {
         setNotes((prev) => [...prev, text]);
     };
- 
+
+    useEffect(() => {
+        localStorage.setItem("notes", JSON.stringify(notes));
+    }, [notes]);
+
 
     return (
         <>
